2831d050d39097d1d304b7684998d06119086e43
sali72 <sahashemi072@gmail.com>
2024-11-05 17:44:52 +0330

    Add Asset and AssetType models
    also modify existing models

--- models/models.py
+++ models/models.py
@@ -5,33 +5,20 @@ from mongoengine import (
     DateTimeField,
     DecimalField,
     Document,
-    EmailField,
-    FloatField,
-    IntField,
+    ListField,
     ReferenceField,
     StringField,
+    ValidationError,
+    EmailField,
 )
 
 
 class User(Document):
-    # user_id = StringField(primary_key=True)
     name = StringField(required=False, max_length=50)
     username = StringField(required=True, unique=True)
-    # email = EmailField(required=True, unique=True)
     email = StringField(required=True, unique=True)
     hashed_password = StringField(required=True)
     role = StringField(required=True, choices=["user", "admin"])
-    created_at = DateTimeField(default=datetime.utcnow)
-    updated_at = DateTimeField(default=datetime.utcnow)
-
-
-class Wallet(Document):
-    wallet_id = StringField(primary_key=True)
-    user_id = ReferenceField(User, required=True)
-    currency_id = ReferenceField("Currency", required=True)
-    name = StringField(required=True, max_length=50)
-    balance = DecimalField(required=True, precision=2)
-    is_predefined = BooleanField(default=False)
     created_at = DateTimeField(default=datetime.utcnow)
     updated_at = DateTimeField(default=datetime.utcnow)
 
@@ -42,18 +29,33 @@ class Currency(Document):
     name = StringField(required=True, max_length=50)
     symbol = StringField(required=True, max_length=5)
     is_predefined = BooleanField(default=False)
+    is_base_currency = BooleanField(default=False)
+    currency_type = StringField(required=True, choices=["fiat", "crypto"])
+
+    def save(self, *args, **kwargs):
+        if self.is_base_currency:
+            if Currency.objects(is_base_currency=True).count() > 0:
+                raise ValidationError("Only one currency can be the base currency.")
+        return super(Currency, self).save(*args, **kwargs)
+
+
+class Wallet(Document):
+    user_id = ReferenceField(User, required=True)
+    name = StringField(unique=True, required=True, max_length=50)
+    type = StringField(required=True, choices=["fiat", "crypto"])
+    currency_ids = ListField(ReferenceField(Currency), required=True)
+    created_at = DateTimeField(default=datetime.utcnow)
+    updated_at = DateTimeField(default=datetime.utcnow)
 
 
 class CurrencyExchange(Document):
-    exchange_id = StringField(primary_key=True)
     from_currency_id = ReferenceField(Currency, required=True)
     to_currency_id = ReferenceField(Currency, required=True)
-    rate = FloatField(required=True)
+    rate = DecimalField(required=True)
     date = DateTimeField(default=datetime.utcnow)
 
 
 class Transaction(Document):
-    transaction_id = StringField(primary_key=True)
     user_id = ReferenceField(User, required=True)
     wallet_id = ReferenceField(Wallet, required=True)
     category_id = ReferenceField("Category", required=True)
@@ -64,6 +66,20 @@ class Transaction(Document):
 
 
 class Category(Document):
-    category_id = StringField(primary_key=True)
     name = StringField(required=True, max_length=50)
     is_predefined = BooleanField(default=False)
+
+
+class AssetType(Document):
+    name = StringField(required=True, unique=True, max_length=50)
+    is_predefined = BooleanField(default=False)
+
+
+class Asset(Document):
+    asset_type = ReferenceField(AssetType, required=True)
+    name = StringField(required=True, max_length=50)
+    description = StringField(required=False, max_length=255)
+    value = DecimalField(required=True, precision=10)
+    user_id = ReferenceField(User, required=True)
+    created_at = DateTimeField(default=datetime.utcnow)
+    updated_at = DateTimeField(default=datetime.utcnow)

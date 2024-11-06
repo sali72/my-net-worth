5a160c2cab6b771bd240e7d5e43f756f32765bfc
sali72 <sahashemi072@gmail.com>
2024-11-06 20:28:44 +0330

    refactor: now wallet_controller methods uses classmethods for crud operations

--- app/api/controllers/wallet_controller.py
+++ app/api/controllers/wallet_controller.py
@@ -1,5 +1,4 @@
 from app.crud.wallet_crud import WalletCRUD
-from app.crud.user_crud import UserCRUD
 from app.crud.currency_crud import CurrencyCRUD
 from models.models import Wallet, User, CurrencyBalance
 from models.schemas import WalletCreateSchema, CurrencyBalanceSchema
@@ -8,14 +7,10 @@ from typing import List
 
 class WalletController:
 
-    wallet_crud = WalletCRUD()
-    user_crud = UserCRUD()
-    currency_crud = CurrencyCRUD()
-
     @classmethod
     async def create_wallet(cls, wallet_schema: WalletCreateSchema, user: User) -> str:
         wallet = await cls.__create_wallet_object(wallet_schema, user.id)
-        wallet_in_db: Wallet = await cls.wallet_crud.create_one(wallet)
+        wallet_in_db: Wallet = await WalletCRUD.create_one(wallet)
         return wallet_in_db.to_dict()
 
     @classmethod
@@ -52,12 +47,12 @@ class WalletController:
 
     @classmethod
     async def get_wallet(cls, wallet_id: str, user_id: str) -> Wallet:
-        wallet = await cls.wallet_crud.get_one_by_user(wallet_id, user_id)
+        wallet = await WalletCRUD.get_one_by_user(wallet_id, user_id)
         return wallet.to_dict()
 
     @classmethod
     async def get_all_wallets(cls, user_id: str) -> List[dict]:
-        wallets: List[Wallet] = await cls.wallet_crud.get_all_by_user_id_optional(
+        wallets: List[Wallet] = await WalletCRUD.get_all_by_user_id_optional(
             user_id
         )
         wallets_list = [wallet.to_dict() for wallet in wallets]
@@ -68,8 +63,8 @@ class WalletController:
         cls, wallet_id: str, wallet_schema: WalletCreateSchema, user_id: str
     ) -> Wallet:
         updated_wallet = cls.__create_wallet_for_update(wallet_schema)
-        await cls.wallet_crud.update_one_by_user(user_id, wallet_id, updated_wallet)
-        return await cls.wallet_crud.get_one_by_user(wallet_id)
+        await WalletCRUD.update_one_by_user(user_id, wallet_id, updated_wallet)
+        return await WalletCRUD.get_one_by_user(wallet_id)
 
     @classmethod
     def __create_wallet_for_update(cls, wallet_schema) -> Wallet:
@@ -82,5 +77,5 @@ class WalletController:
 
     @classmethod
     async def delete_wallet(cls, wallet_id: str, user_id: str) -> bool:
-        await cls.wallet_crud.delete_one_by_user(user_id, wallet_id)
+        await WalletCRUD.delete_one_by_user(user_id, wallet_id)
         return True

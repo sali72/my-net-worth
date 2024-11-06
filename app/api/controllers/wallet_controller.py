2387b57ddc095419c6f434da8a1e1dbe801e1441
sali72 <sahashemi072@gmail.com>
2024-11-06 17:47:45 +0330

    Add other crud routs and needed controller methods
    manually tested get all wallets route

--- app/api/controllers/wallet_controller.py
+++ app/api/controllers/wallet_controller.py
@@ -3,6 +3,7 @@ from app.crud.user_crud import UserCRUD
 from app.crud.currency_crud import CurrencyCRUD
 from models.models import Wallet, User
 from models.schemas import WalletSchema
+from typing import List
 
 
 class WalletController:
@@ -15,7 +16,7 @@ class WalletController:
     async def create_wallet(cls, wallet_schema: WalletSchema, user: User) -> str:
         wallet = await cls.__create_wallet(wallet_schema, user.id)
         wallet_in_db: Wallet = await cls.wallet_crud.create_one(wallet)
-        return str(wallet_in_db.id)
+        return wallet_in_db.to_dict()
 
     @classmethod
     async def __create_wallet(cls, wallet_schema: WalletSchema, user_id) -> Wallet:
@@ -28,3 +29,36 @@ class WalletController:
             type=wallet_schema.type,
             currency_ids=currencies,
         )
+
+    @classmethod
+    async def get_wallet(cls, wallet_id: str, user_id: str) -> Wallet:
+        wallet = await cls.wallet_crud.get_one_by_user(wallet_id, user_id)
+        return wallet
+
+    @classmethod
+    async def get_all_wallets(cls, user_id: str) -> List[dict]:
+        wallets: List[Wallet] = await cls.wallet_crud.get_all_by_user_id_optional(user_id)
+        wallets_list = [wallet.to_dict() for wallet in wallets]
+        return wallets_list
+
+    @classmethod
+    async def update_wallet(
+        cls, wallet_id: str, wallet_schema: WalletSchema, user_id: str
+    ) -> Wallet:
+        updated_wallet = cls.__create_wallet_for_update(wallet_schema)
+        await cls.wallet_crud.update_one_by_user(user_id, wallet_id, updated_wallet)
+        return await cls.wallet_crud.get_one_by_user(wallet_id)
+
+    @classmethod
+    def __create_wallet_for_update(cls, wallet_schema) -> Wallet:
+        updated_wallet = Wallet(
+            name=wallet_schema.name,
+            type=wallet_schema.type,
+            currency_ids=wallet_schema.currency_codes,
+        )
+        return updated_wallet
+
+    @classmethod
+    async def delete_wallet(cls, wallet_id: str, user_id: str) -> bool:
+        await cls.wallet_crud.delete_one_by_user(user_id, wallet_id)
+        return True
--- app/api/endpoints/wallet_routes.py
+++ app/api/endpoints/wallet_routes.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, Depends
-
-from app.api.controllers.auth_controller import has_role
+from fastapi import APIRouter, Depends, HTTPException, Path
 from models.schemas import ResponseSchema, WalletSchema
 from models.schemas import Role as R
+from app.api.controllers.auth_controller import has_role
 from app.api.controllers.wallet_controller import WalletController
 
 router = APIRouter(prefix="/wallets", tags=["Wallet"])
@@ -10,10 +9,51 @@ router = APIRouter(prefix="/wallets", tags=["Wallet"])
 
 @router.post("", response_model=ResponseSchema)
 async def create_wallet_route(
-    wallet_schema: WalletSchema, 
-    user=Depends(has_role(R.USER))
+    wallet_schema: WalletSchema, user=Depends(has_role(R.USER))
 ):
     wallet_id = await WalletController.create_wallet(wallet_schema, user)
     message = "Wallet created successfully"
     data = {"id": wallet_id}
     return ResponseSchema(data=data, message=message)
+
+
+@router.get("/{wallet_id}", response_model=ResponseSchema)
+async def read_wallet_route(
+    wallet_id: str = Path(..., description="The ID of the wallet to retrieve"),
+    user=Depends(has_role(R.USER)),
+):
+    wallet = await WalletController.get_wallet(wallet_id, user.id)
+    return ResponseSchema(
+        data={"wallet": wallet}, message="Wallet retrieved successfully"
+    )
+
+
+@router.get("", response_model=ResponseSchema)
+async def read_all_wallets_route(user=Depends(has_role(R.USER))):
+    wallets = await WalletController.get_all_wallets(user.id)
+    return ResponseSchema(
+        data={"wallets": wallets}, message="Wallets retrieved successfully"
+    )
+
+
+@router.put("/{wallet_id}", response_model=ResponseSchema)
+async def update_wallet_route(
+    wallet_schema: WalletSchema,
+    wallet_id: str = Path(..., description="The ID of the wallet to update"),
+    user=Depends(has_role(R.USER)),
+):
+    updated_wallet = await WalletController.update_wallet(
+        wallet_id, wallet_schema, user.id
+    )
+    return ResponseSchema(
+        data={"wallet": updated_wallet}, message="Wallet updated successfully"
+    )
+
+
+@router.delete("/{wallet_id}", response_model=ResponseSchema)
+async def delete_wallet_route(
+    wallet_id: str = Path(..., description="The ID of the wallet to delete"),
+    user=Depends(has_role(R.USER)),
+):
+    success = await WalletController.delete_wallet(wallet_id, user.id)
+    return ResponseSchema(message="Wallet deleted successfully")

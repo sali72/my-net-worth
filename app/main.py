42fe99db026afaaa56eb14cc94bcf18ee650b01a
sali72 <sahashemi072@gmail.com>
2024-11-10 17:49:45 +0330

    fix: error messages codes are fixed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from app.api.endpoints.authentication_routes import router as auth_routes
 from app.api.endpoints.wallet_routes import router as wallet_routes
-from commons.exception_handlers import base_exception_handler
+from commons.exception_handlers import base_exception_handler, http_exception_handler
 from database.database import connect_to_db
 
 
@@ -24,4 +24,5 @@ app.include_router(auth_routes)
 app.include_router(wallet_routes)
 
 
+app.add_exception_handler(HTTPException, http_exception_handler)
 app.add_exception_handler(Exception, base_exception_handler)
--- commons/exception_handlers.py
+++ commons/exception_handlers.py
@@ -1,13 +1,43 @@
 from fastapi.responses import JSONResponse
-import traceback
+from fastapi import HTTPException
 
+exception_status_map = {
+    # Built-in Python exception
+    "ValueError": 400,
+    "KeyError": 404,
+    # mongoengine.errors
+    "ValidationError": 422,
+    "DoesNotExist": 404,
+    "NotUniqueError": 409,
+    # bson.errors
+    "InvalidId": 400,
+}
+
+def get_exception_name(exc: Exception) -> str:
+    """Get the name of the exception."""
+    return type(exc).__name__
+
+def get_status_code(exception_name: str) -> int:
+    """Get the status code based on the exception name."""
+    return exception_status_map.get(exception_name, 500)
 
 async def base_exception_handler(request, exc: Exception):
+    exception_name = get_exception_name(exc)
+    status_code = get_status_code(exception_name)
+
     return JSONResponse(
-        status_code=404,
+        status_code=status_code,
         content={
-            "status": "error",
-            "exception_name": type(exc).__name__,
-            "message": str(exc),
+            "exception_name": exception_name,
+            "detail": str(exc),
+        },
+    )
+
+async def http_exception_handler(request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "exception_name": "HTTPException",
+            "detail": exc.detail,
         },
     )

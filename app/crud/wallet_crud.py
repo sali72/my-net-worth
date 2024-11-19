c28592592a3db951256051705d415ebe6ae2c2be
sali72 <sahashemi072@gmail.com>
2024-11-19 13:41:13 +0330

    feat: add and remove currency balance in crud of wallet

--- app/crud/wallet_crud.py
+++ app/crud/wallet_crud.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from fastapi import HTTPException, status
 from mongoengine import DoesNotExist, QuerySet
 
-from models.models import Wallet
+from models.models import Wallet, CurrencyBalance
 
 
 class WalletCRUD:
@@ -48,11 +48,15 @@ class WalletCRUD:
 
     @staticmethod
     def __update_currency_balances(wallet: Wallet, updated_wallet: Wallet):
+        # it does not add or remove a currency balance
         if updated_wallet.currency_balances is not None:
             for updated_balance in updated_wallet.currency_balances:
                 match_found = False
                 for existing_balance in wallet.currency_balances:
-                    if existing_balance.currency_id.pk == updated_balance.currency_id.pk:
+                    if (
+                        existing_balance.currency_id.pk
+                        == updated_balance.currency_id.pk
+                    ):
                         existing_balance.balance = updated_balance.balance
                         match_found = True
                 if not match_found:
@@ -73,3 +77,29 @@ class WalletCRUD:
                 f"Wallet with id {wallet_id} for user {user_id} does not exist"
             )
         return result > 0
+
+    @classmethod
+    async def add_currency_balance_to_wallet(
+        cls, user_id: str, wallet_id: str, currency_balance: CurrencyBalance
+    ) -> Wallet:
+        wallet = await cls.get_one_by_user(wallet_id, user_id)
+        wallet.currency_balances.append(currency_balance)
+        wallet.save()
+        return wallet
+    
+    @classmethod
+    async def remove_currency_balance_from_wallet(
+        cls, user_id: str, wallet_id: str, currency_id: str
+    ) -> Wallet:
+        wallet = await cls.get_one_by_user(wallet_id, user_id)
+        
+        for existing_balance in wallet.currency_balances:
+            if str(existing_balance.currency_id.pk) == currency_id:
+                wallet.currency_balances.remove(existing_balance)
+                wallet.save()
+                return wallet
+
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Currency balance not found in the wallet",
+        )

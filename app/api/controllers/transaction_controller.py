5c0ff7d9b0d65b5553acb6c1d772c4144ce0eb41
sali72 <sahashemi072@gmail.com>
2024-12-02 17:34:47 +0330

    refactor: transaction_controller.py reduced code size drastically

--- app/api/controllers/transaction_controller.py
+++ app/api/controllers/transaction_controller.py
@@ -12,17 +12,15 @@ from models.schemas import TransactionCreateSchema, TransactionUpdateSchema
 
 
 class TransactionController:
-
     @classmethod
     async def create_transaction(
         cls, transaction_schema: TransactionCreateSchema, user: User
     ) -> Transaction:
         await cls._validate_transaction_data(transaction_schema, user.id)
         transaction = cls._create_transaction_obj_to_create(transaction_schema, user.id)
-
         transaction_in_db: Transaction = await TransactionCRUD.create_one(transaction)
         try:
-            await cls._update_wallet_balances(transaction_in_db, user.id)
+            await cls._adjust_wallet_balances(transaction_in_db, user.id)
             return transaction_in_db
         except Exception as e:
             await TransactionCRUD.delete_one_by_user(transaction_in_db.id, user.id)
@@ -53,14 +51,16 @@ class TransactionController:
         updated_transaction = cls._create_transaction_obj_for_update(
             transaction_update_schema
         )
-
         await TransactionCRUD.update_one_by_user(
             user_id, transaction_id, updated_transaction
         )
 
         if updated_transaction.amount:
-            await cls._update_wallet_amount_differences(
-                transaction_id, user_id, existing_transaction, updated_transaction
+            await cls._handle_amount_update(
+                existing_transaction,
+                updated_transaction.amount,
+                user_id,
+                transaction_id,
             )
 
         transaction_from_db = await TransactionCRUD.get_one_by_id(transaction_id)
@@ -71,7 +71,7 @@ class TransactionController:
         transaction = await TransactionCRUD.get_one_by_user(transaction_id, user_id)
         await TransactionCRUD.delete_one_by_user(transaction_id, user_id)
         try:
-            await cls._reverse_transaction_effects(transaction, user_id)
+            await cls._adjust_wallet_balances(transaction, user_id, multiplier=-1)
             return transaction
         except Exception as e:
             await TransactionCRUD.create_one(transaction)
@@ -97,7 +97,6 @@ class TransactionController:
             from_wallet_id,
             to_wallet_id,
         )
-
         return [transaction.to_dict() for transaction in transactions]
 
     @classmethod
@@ -107,18 +106,8 @@ class TransactionController:
         transactions = await TransactionCRUD.filter_transactions(
             user_id, start_date, end_date
         )
-
-        total_income = Decimal(0)
-        total_expense = Decimal(0)
-
-        for transaction in transactions:
-            if transaction.type == "income":
-                total_income += transaction.amount
-            elif transaction.type == "expense":
-                total_expense += transaction.amount
-
+        total_income, total_expense = cls._calculate_income_expense(transactions)
         net_balance = total_income - total_expense
-
         return {
             "total_income": total_income,
             "total_expense": total_expense,
@@ -131,50 +120,37 @@ class TransactionController:
     ) -> None:
         if transaction_schema.category_id:
             await CategoryCRUD.get_one_by_user(transaction_schema.category_id, user_id)
-
-        await cls._check_currency_existence_in_wallets(transaction_schema, user_id)
+        await cls._check_currency_in_wallets(transaction_schema, user_id)
 
     @classmethod
-    async def _check_currency_existence_in_wallets(
+    async def _check_currency_in_wallets(
         cls, transaction_schema: TransactionCreateSchema, user_id: str
     ) -> None:
-        if transaction_schema.type == "income":
-            to_wallet = await WalletCRUD.get_one_by_user(
-                transaction_schema.to_wallet_id, user_id
-            )
-            if not cls._currency_exists_in_wallet(
-                to_wallet, transaction_schema.currency_id
-            ):
+        wallets = await cls._get_wallets_for_transaction(transaction_schema, user_id)
+        currency_id = transaction_schema.currency_id
+
+        for wallet in wallets:
+            if not cls._currency_exists_in_wallet(wallet, currency_id):
                 raise ValidationError(
-                    "Currency does not exist in the destination wallet for income transactions."
+                    f"Currency does not exist in the wallet {wallet.id} for transaction type '{transaction_schema.type}'."
                 )
 
-        elif transaction_schema.type == "expense":
+    @classmethod
+    async def _get_wallets_for_transaction(
+        cls, transaction_schema: TransactionCreateSchema, user_id: str
+    ) -> List[Wallet]:
+        wallets = []
+        if transaction_schema.type in ["expense", "transfer"]:
             from_wallet = await WalletCRUD.get_one_by_user(
                 transaction_schema.from_wallet_id, user_id
             )
-            if not cls._currency_exists_in_wallet(
-                from_wallet, transaction_schema.currency_id
-            ):
-                raise ValidationError(
-                    "Currency does not exist in the source wallet for expense transactions."
-                )
-
-        elif transaction_schema.type == "transfer":
-            from_wallet = await WalletCRUD.get_one_by_user(
-                transaction_schema.from_wallet_id, user_id
-            )
+            wallets.append(from_wallet)
+        if transaction_schema.type in ["income", "transfer"]:
             to_wallet = await WalletCRUD.get_one_by_user(
                 transaction_schema.to_wallet_id, user_id
             )
-            if not cls._currency_exists_in_wallet(
-                from_wallet, transaction_schema.currency_id
-            ) or not cls._currency_exists_in_wallet(
-                to_wallet, transaction_schema.currency_id
-            ):
-                raise ValidationError(
-                    "Currency must exist in both source and destination wallets for transfer transactions."
-                )
+            wallets.append(to_wallet)
+        return wallets
 
     @staticmethod
     def _currency_exists_in_wallet(wallet: Wallet, currency_id: str) -> bool:
@@ -184,151 +160,60 @@ class TransactionController:
         )
 
     @classmethod
-    async def _update_wallet_balances(
-        cls, transaction: Transaction, user_id: str
+    async def _adjust_wallet_balances(
+        cls,
+        transaction: Transaction,
+        user_id: str,
+        amount: Decimal = None,
+        multiplier: int = 1,
     ) -> None:
+        if amount is None:
+            amount = transaction.amount
+        amount *= multiplier
+
+        adjustments = cls._get_balance_adjustments(transaction, amount)
+        for wallet_id, currency_id, amt in adjustments:
+            await cls._adjust_wallet_balance(wallet_id, currency_id, amt, user_id)
+
+    @classmethod
+    def _get_balance_adjustments(
+        cls, transaction: Transaction, amount: Decimal
+    ) -> List[tuple]:
+        adjustments = []
         if transaction.type == "income":
-            await cls._get_wallet_and_update_balance(
-                transaction.to_wallet_id.id,
-                transaction.currency_id.id,
-                transaction.amount,
-                user_id,
-                add=True,
+            adjustments.append(
+                (transaction.to_wallet_id.id, transaction.currency_id.id, amount)
             )
         elif transaction.type == "expense":
-            await cls._get_wallet_and_update_balance(
-                transaction.from_wallet_id.id,
-                transaction.currency_id.id,
-                transaction.amount,
-                user_id,
-                add=False,
+            adjustments.append(
+                (transaction.from_wallet_id.id, transaction.currency_id.id, -amount)
             )
         elif transaction.type == "transfer":
-            await cls._update_wallet_balance_transfer(transaction, user_id)
-
-    @classmethod
-    async def _update_wallet_balance_transfer(
-        cls, transaction: Transaction, user_id: str
-    ) -> None:
-        from_wallet, to_wallet = await cls._fetch_and_validate_wallets_for_transaction(
-            transaction, user_id
-        )
-
-        await cls._update_wallet_balance(
-            from_wallet,
-            transaction.from_wallet_id.id,
-            transaction.currency_id.id,
-            transaction.amount,
-            user_id,
-            add=False,
-        )
-        await cls._update_wallet_balance(
-            to_wallet,
-            transaction.to_wallet_id.id,
-            transaction.currency_id.id,
-            transaction.amount,
-            user_id,
-            add=True,
-        )
-
-    @classmethod
-    async def _fetch_and_validate_wallets_for_transaction(
-        cls, transaction: Transaction, user_id: str
-    ) -> Tuple[Wallet, Wallet]:
-        from_wallet = await WalletCRUD.get_one_by_user(
-            transaction.from_wallet_id.id, user_id
-        )
-        to_wallet = await WalletCRUD.get_one_by_user(
-            transaction.to_wallet_id.id, user_id
-        )
-
-        if not cls._validate_currency_match(
-            from_wallet, to_wallet, transaction.currency_id.id
-        ):
-            raise ValidationError(
-                "Currency of the source wallet must match the destination wallet for transfers."
+            adjustments.append(
+                (transaction.from_wallet_id.id, transaction.currency_id.id, -amount)
             )
-
-        return from_wallet, to_wallet
-
-    @staticmethod
-    def _validate_currency_match(
-        from_wallet: Wallet, to_wallet: Wallet, currency_id: str
-    ) -> bool:
-        from_currency_match = any(
-            balance.currency_id.pk == currency_id
-            for balance in from_wallet.balances_ids
-        )
-        to_currency_match = any(
-            balance.currency_id.pk == currency_id for balance in to_wallet.balances_ids
-        )
-        return from_currency_match and to_currency_match
+            adjustments.append(
+                (transaction.to_wallet_id.id, transaction.currency_id.id, amount)
+            )
+        return adjustments
 
     @classmethod
-    async def _get_wallet_and_update_balance(
-        cls, wallet_id: str, currency_id: str, amount: float, user_id: str, add: bool
+    async def _adjust_wallet_balance(
+        cls, wallet_id: str, currency_id: str, amount: Decimal, user_id: str
     ) -> None:
         wallet = await WalletCRUD.get_one_by_user(wallet_id, user_id)
-
-        await cls._update_wallet_balance(
-            wallet,
-            wallet_id,
-            currency_id,
-            amount,
-            user_id,
-            add,
-        )
-
-    @classmethod
-    async def _update_wallet_balance(
-        cls,
-        wallet: Wallet,
-        wallet_id: str,
-        currency_id: str,
-        amount: float,
-        user_id: str,
-        add: bool,
-    ) -> None:
-
-        if not add:
-            await cls._validate_balance_sufficiency(wallet, currency_id, amount)
-
-        cls._adjust_balance_amount(currency_id, amount, add, wallet)
-
+        cls._adjust_balance(wallet, currency_id, amount)
         await WalletCRUD.update_one_by_user(user_id, wallet_id, wallet)
 
     @classmethod
-    def _adjust_balance_amount(
-        cls, currency_id: str, amount: float, add: bool, wallet: Wallet
-    ) -> None:
+    def _adjust_balance(cls, wallet: Wallet, currency_id: str, amount: Decimal) -> None:
         for balance in wallet.balances_ids:
             if balance.currency_id.pk == currency_id:
-                if add:
-                    balance.amount += amount
-                else:
-                    balance.amount -= amount
-                break
-
-    @classmethod
-    async def _validate_balance_sufficiency(
-        cls,
-        wallet: Wallet,
-        currency_id: str,
-        amount: float,
-    ) -> None:
-        if not await cls._has_sufficient_balance(wallet, currency_id, amount):
-            raise ValidationError(
-                "Insufficient balance in the wallet for this transaction."
-            )
-
-    @classmethod
-    async def _has_sufficient_balance(
-        cls, wallet: Wallet, currency_id: str, amount: float
-    ) -> bool:
-        for balance in wallet.balances_ids:
-            if balance.currency_id.pk == currency_id:
-                return balance.amount >= amount
-        return False
+                balance.amount += amount
+                if balance.amount < 0:
+                    raise ValidationError("Insufficient balance in the wallet.")
+                return
+        raise ValidationError("Currency does not exist in the wallet.")
 
     @classmethod
     def _create_transaction_obj_to_create(
@@ -358,135 +243,42 @@ class TransactionController:
         )
 
     @classmethod
-    async def _update_wallet_amount_differences(
+    async def _handle_amount_update(
         cls,
-        transaction_id: str,
-        user_id: str,
         existing_transaction: Transaction,
-        updated_transaction: Transaction,
+        new_amount: Decimal,
+        user_id: str,
+        transaction_id: str,
     ) -> None:
-        amount_difference = updated_transaction.amount - existing_transaction.amount
-
+        amount_difference = new_amount - existing_transaction.amount
         if amount_difference != 0:
             try:
-                await cls._adjust_wallet_balances_for_update(
-                    existing_transaction,
-                    user_id,
-                    amount_difference,
+                await cls._adjust_wallet_balances(
+                    existing_transaction, user_id, amount=amount_difference
                 )
             except Exception as e:
-                await TransactionCRUD.update_one_by_user(
+                await cls._rollback_transaction_update(
                     user_id, transaction_id, existing_transaction
                 )
                 raise e
 
     @classmethod
-    async def _adjust_wallet_balances_for_update(
-        cls,
-        existing_transaction: Transaction,
-        user_id: str,
-        amount_difference: float,
+    async def _rollback_transaction_update(
+        cls, user_id: str, transaction_id: str, existing_transaction: Transaction
     ) -> None:
-        if existing_transaction.type == "income":
-            await cls._get_wallet_and_update_balance(
-                existing_transaction.to_wallet_id.id,
-                existing_transaction.currency_id.id,
-                abs(amount_difference),
-                user_id,
-                add=(amount_difference > 0),
-            )
-        elif existing_transaction.type == "expense":
-            await cls._get_wallet_and_update_balance(
-                existing_transaction.from_wallet_id.id,
-                existing_transaction.currency_id.id,
-                abs(amount_difference),
-                user_id,
-                add=(amount_difference < 0),
-            )
-        elif existing_transaction.type == "transfer":
-            await cls._adjust_transfer_wallet_balances_for_update(
-                existing_transaction, user_id, amount_difference
-            )
-
-    @classmethod
-    async def _adjust_transfer_wallet_balances_for_update(
-        cls,
-        existing_transaction: Transaction,
-        user_id: str,
-        amount_difference: float,
-    ) -> None:
-        await cls._get_wallet_and_update_balance(
-            existing_transaction.from_wallet_id.id,
-            existing_transaction.currency_id.id,
-            abs(amount_difference),
-            user_id,
-            add=(amount_difference < 0),
-        )
-        await cls._get_wallet_and_update_balance(
-            existing_transaction.to_wallet_id.id,
-            existing_transaction.currency_id.id,
-            abs(amount_difference),
-            user_id,
-            add=(amount_difference > 0),
+        await TransactionCRUD.update_one_by_user(
+            user_id, transaction_id, existing_transaction
         )
 
-    @classmethod
-    async def _reverse_transaction_effects(
-        cls, transaction: Transaction, user_id: str
-    ) -> None:
-        if transaction.type == "income":
-            await cls._reverse_income_effect(transaction, user_id)
-        elif transaction.type == "expense":
-            await cls._reverse_expense_effect(transaction, user_id)
-        elif transaction.type == "transfer":
-            await cls._reverse_wallet_balance_transfer(transaction, user_id)
-
-    @classmethod
-    async def _reverse_income_effect(
-        cls, transaction: Transaction, user_id: str
-    ) -> None:
-        await cls._get_wallet_and_update_balance(
-            transaction.to_wallet_id.id,
-            transaction.currency_id.id,
-            transaction.amount,
-            user_id,
-            add=False,  # Reverse the addition
-        )
-
-    @classmethod
-    async def _reverse_expense_effect(
-        cls, transaction: Transaction, user_id: str
-    ) -> None:
-        await cls._get_wallet_and_update_balance(
-            transaction.from_wallet_id.id,
-            transaction.currency_id.id,
-            transaction.amount,
-            user_id,
-            add=True,  # Reverse the subtraction
-        )
-
-    @classmethod
-    async def _reverse_wallet_balance_transfer(
-        cls, transaction: Transaction, user_id: str
-    ) -> None:
-        from_wallet, to_wallet = await cls._fetch_and_validate_wallets_for_transaction(
-            transaction, user_id
-        )
-
-        # Reverse the transfer: add back to the source and subtract from the destination
-        await cls._update_wallet_balance(
-            from_wallet,
-            transaction.from_wallet_id.id,
-            transaction.currency_id.id,
-            transaction.amount,
-            user_id,
-            add=True,  # Reverse the subtraction
-        )
-        await cls._update_wallet_balance(
-            to_wallet,
-            transaction.to_wallet_id.id,
-            transaction.currency_id.id,
-            transaction.amount,
-            user_id,
-            add=False,  # Reverse the addition
-        )
+    @staticmethod
+    def _calculate_income_expense(
+        transactions: List[Transaction],
+    ) -> Tuple[Decimal, Decimal]:
+        total_income = Decimal(0)
+        total_expense = Decimal(0)
+        for transaction in transactions:
+            if transaction.type == "income":
+                total_income += transaction.amount
+            elif transaction.type == "expense":
+                total_expense += transaction.amount
+        return total_income, total_expense

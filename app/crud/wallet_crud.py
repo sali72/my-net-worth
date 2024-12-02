14daa2e6ce04efdfa4ea232b699abb4506718c5b
sali72 <sahashemi072@gmail.com>
2024-12-02 12:14:23 +0330

    refactor: wallet_crud.py

--- app/crud/wallet_crud.py
+++ app/crud/wallet_crud.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from typing import List
-
+from typing import List, Optional
 from fastapi import HTTPException, status
 from mongoengine import DoesNotExist, QuerySet
 from app.crud.balance_crud import BalanceCRUD
@@ -10,31 +9,44 @@ from models.models import Balance, Wallet
 class WalletCRUD:
 
     @classmethod
-    async def create_one(cls, user: Wallet) -> Wallet:
-        user.save()
-        return user
+    async def create_one(cls, wallet: Wallet) -> Wallet:
+        wallet.save()
+        return wallet
 
     @classmethod
-    async def get_one_by_user_optional(cls, wallet_id: str, user_id: str) -> Wallet:
+    async def get_one_by_user_optional(
+        cls, wallet_id: str, user_id: str
+    ) -> Optional[Wallet]:
         return Wallet.objects(id=wallet_id, user_id=user_id).first()
 
     @classmethod
     async def get_one_by_user(cls, wallet_id: str, user_id: str) -> Wallet:
-        return Wallet.objects.get(id=wallet_id, user_id=user_id)
+        try:
+            return Wallet.objects.get(id=wallet_id, user_id=user_id)
+        except DoesNotExist:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Wallet with id {wallet_id} for user {user_id} does not exist",
+            )
 
     @classmethod
     async def get_all_by_user_id_optional(cls, user_id: str) -> QuerySet:
-        wallets = Wallet.objects(user_id=user_id)
-        return wallets
+        return Wallet.objects(user_id=user_id)
 
     @classmethod
     async def get_one_by_id(cls, wallet_id: str) -> Wallet:
-        return Wallet.objects.get(id=wallet_id)
+        try:
+            return Wallet.objects.get(id=wallet_id)
+        except DoesNotExist:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Wallet with id {wallet_id} does not exist",
+            )
 
     @classmethod
     async def update_one_by_user(
         cls, user_id: str, wallet_id: str, updated_wallet: Wallet
-    ):
+    ) -> None:
         wallet = await cls.get_one_by_user(wallet_id, user_id)
         cls.__update_wallet_fields(wallet, updated_wallet)
         await cls.__update_balances(wallet, updated_wallet)
@@ -42,14 +54,12 @@ class WalletCRUD:
         wallet.save()
 
     @staticmethod
-    def __update_wallet_fields(wallet: Wallet, updated_wallet: Wallet):
+    def __update_wallet_fields(wallet: Wallet, updated_wallet: Wallet) -> None:
         if updated_wallet.name is not None:
             wallet.name = updated_wallet.name
 
     @staticmethod
-    async def __update_balances(wallet: Wallet, updated_wallet: Wallet):
-        # this method only can update a balance
-        # it does not add or remove a currency balance
+    async def __update_balances(wallet: Wallet, updated_wallet: Wallet) -> None:
         if updated_wallet.balances_ids is not None:
             updated_balances: List[Balance] = updated_wallet.balances_ids
             for updated_balance in updated_balances:
@@ -68,19 +78,20 @@ class WalletCRUD:
                 if not match_found:
                     raise HTTPException(
                         status_code=status.HTTP_404_NOT_FOUND,
-                        detail=f"Currency ids provided are not related to this user",
+                        detail="Currency ids provided are not related to this user",
                     )
 
     @staticmethod
-    def __update_timestamp(wallet: Wallet):
+    def __update_timestamp(wallet: Wallet) -> None:
         wallet.updated_at = datetime.utcnow()
 
     @classmethod
     async def delete_one_by_user(cls, user_id: str, wallet_id: str) -> bool:
         result = Wallet.objects(id=wallet_id, user_id=user_id).delete()
         if result == 0:
-            raise DoesNotExist(
-                f"Wallet with id {wallet_id} for user {user_id} does not exist"
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Wallet with id {wallet_id} for user {user_id} does not exist",
             )
         return result > 0
 
@@ -105,7 +116,7 @@ class WalletCRUD:
 
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Currency balance not found in the wallet.",
+            detail="Balance not found in the wallet.",
         )
 
     @classmethod
@@ -122,5 +133,5 @@ class WalletCRUD:
 
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Currency balance not found in the wallet",
+            detail="Balance not found in the wallet",
         )

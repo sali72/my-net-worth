34549fa37e4a79a38cbb07ece91c7da07ae049bc
sali72 <sahashemi072@gmail.com>
2024-12-02 12:02:40 +0330

    refactor: user_crud.py
    consistent style across the project

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -1,29 +1,38 @@
+from typing import List, Optional
+
 from models.models import User
 
 
 class UserCRUD:
 
-    async def create_one(self, user: User) -> User:
+    @classmethod
+    async def create_one(cls, user: User) -> User:
         user.save()
         return user
 
-    async def get_all(self):
+    @classmethod
+    async def get_all(cls) -> List[User]:
         return list(User.objects)
 
-    async def get_one_by_username(self, username: str) -> User:
+    @classmethod
+    async def get_one_by_username(cls, username: str) -> User:
         return User.objects.get(username=username)
 
-    async def get_one_by_username_optional(self, username: str) -> User:
+    @classmethod
+    async def get_one_by_username_optional(cls, username: str) -> Optional[User]:
         return User.objects(username=username).first()
 
-    async def get_one_by_id(self, user_id: str) -> User:
+    @classmethod
+    async def get_one_by_id(cls, user_id: str) -> User:
         return User.objects.get(user_id=user_id)
 
-    async def update_one(self, username: str, updated_user: User) -> int:
-        user = await self.get_one_by_username(username=username)
+    @classmethod
+    async def update_one(cls, username: str, updated_user: User) -> int:
+        user = await cls.get_one_by_username(username=username)
         user.update(**updated_user)
         return user.save()
 
-    async def delete_one(self, username: str) -> int:
-        user = await self.get_one_by_username(username=username)
+    @classmethod
+    async def delete_one(cls, username: str) -> int:
+        user = await cls.get_one_by_username(username=username)
         return user.delete()
